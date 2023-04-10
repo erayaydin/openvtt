e187ab44c16e1b4b7d1e826623310423f8a078df
Eray Aydın <erayaydinn@protonmail.com>
2023-04-10 17:09:42 +0300

    feat: cache and queue laravel service providers

--- config/app.php
+++ config/app.php
@@ -158,9 +158,11 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
+        Illuminate\Cache\CacheServiceProvider::class,
         Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
         Illuminate\Database\DatabaseServiceProvider::class,
         Illuminate\Filesystem\FilesystemServiceProvider::class,
+        Illuminate\Queue\QueueServiceProvider::class,
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
